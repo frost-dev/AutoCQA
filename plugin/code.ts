ec49b6e0b7f4c16bc6effa4232e267bf5dbb74a8
lxpocampo <enzo.ocampo@frostdesigngroup.com>
2024-04-25 17:35:53 +0800

    Updated main
    Added code for plugin and separated folder for backend and plugin.

---
+++ plugin/code.ts
@@ -0,0 +1,48 @@
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
+
+figma.ui.resize(500,500);
+
+figma.ui.onmessage =  async (urlInput: string) => {
+  let text = ""
+  for (const node of figma.currentPage.selection) {
+    if ("characters" in node) {
+      text = node.characters;
+    }
+  }
+
+  const response = await fetch(`http://localhost:3000/verify-content?siteURL="${urlInput}"&figmaContent="${encodeURIComponent(text)}"`);
+  const data = await response.json();
+  console.log('The API: ', data.gpt_correction.content);
+  
+  figma.ui.postMessage({
+    web: data.gpt_correction.siteContent ? data.gpt_correction.siteContent : 'Does not exist',
+    correction: data.gpt_correction.content ? data.gpt_correction.content : 'Content is correct!'
+  });
+};
+
+
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// // posted message.
+// figma.ui.onmessage =  (msg: {type: string, count: number}) => {
+//   // One way of distinguishing between different types of messages sent from
+//   // your HTML page is to use an object with a "type" property like this.
+//   if (msg.type === 'create-rectangles') {
+//     const nodes: SceneNode[] = [];
+//     for (let i = 0; i < msg.count; i++) {
+//       const rect = figma.createRectangle();
+//       rect.x = i * 150;
+//       rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+//       figma.currentPage.appendChild(rect);
+//       nodes.push(rect);
+//     }
+//     figma.currentPage.selection = nodes;
+//     figma.viewport.scrollAndZoomIntoView(nodes);
+//   }
+
+//   // Make sure to close the plugin when you're done. Otherwise the plugin will
+//   // keep running, which shows the cancel button at the bottom of the screen.
+//   figma.closePlugin();
+// };
+
